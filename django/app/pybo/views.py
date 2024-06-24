4293a26fa2f00d0dc824f8d3aecd92303054a24d
lim <gylim@student.42seoul.kr>
2024-06-24 19:30:05 +0900

    modify index

--- django/app/pybo/views.py
+++ django/app/pybo/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.http import HttpRequest
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("hello, world! from 42")
+    return render(request, "base.html")
 
 # Create your views here.
