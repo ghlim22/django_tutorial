c67b2aa8dc78304c5879fc58e50b330662b9fbbd
Gyuhyun Lim <hyunxx14@gmail.com>
2024-06-27 15:29:59 +0900

    lecture 18

--- django/project/account/urls.py
+++ django/project/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'account'
 urlpatterns = [
     path("", views.index),
-    path("base/", views.index)
+    path("base/", views.index, name="index"),
 ]
--- django/project/account/views.py
+++ django/project/account/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import HttpRequest
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from django.urls import reverse
 
+from account.models import TestProfile
 
 def index(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
-        return render(request, "account/base.html", context={'text': 'POST METHOD!'})
-    return render(request, "account/base.html", context={'text': 'GET METHOD!'})
+        temp = request.POST.get('hello_world_input')
+
+        new_test_model = TestProfile()
+        new_test_model.text = temp
+        new_test_model.save()
+
+        test_list = TestProfile.objects.all()
+
+        return HttpResponseRedirect(reverse('account:index'))
+    test_list = TestProfile.objects.all()
+    return render(request, "account/base.html", context={'test_list': test_list})
