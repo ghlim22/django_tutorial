f0b06d5f6131ac0098d65e34cbe4ba188fc4a630
Gyuhyun Lim <hyunxx14@gmail.com>
2024-06-27 13:41:00 +0900

    lecture 16

--- django/project/account/views.py
+++ django/project/account/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpRequest
 
+
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "account/base.html")
+    if request.method == 'POST':
+        return render(request, "account/base.html", context={'text': 'POST METHOD!'})
+    return render(request, "account/base.html", context={'text': 'GET METHOD!'})
