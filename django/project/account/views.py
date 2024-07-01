81a8f0ce3d9b84e67f438d5737f0ac1bfae4f51b
lim <gylim@student.42seoul.kr>
2024-07-01 16:05:10 +0900

    lecture 24

--- django/project/account/views.py
+++ django/project/account/views.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
-from account.models import TestProfile
+from .models import TestProfile
+
 
 def index(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
@@ -21,8 +22,14 @@ def index(request: HttpRequest) -> HttpResponse:
     test_list = TestProfile.objects.all()
     return render(request, template_name="account/base.html", context={'test_list': test_list})
 
+
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy("account:index")
     template_name = "account/create.html"
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = "account/detail.html"
