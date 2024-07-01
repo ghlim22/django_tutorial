9c458e658e3571a43f50a98186f2ce8125fe11d6
lim <gylim@student.42seoul.kr>
2024-07-01 16:26:53 +0900

    lecture 25

--- django/project/account/views.py
+++ django/project/account/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from .forms import AccountUpdateForm
 from .models import TestProfile
 
 
@@ -33,3 +34,9 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = "account/detail.html"
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm;
+    success_url = reverse_lazy("account:index")
+    template_name = "account/update.html"
